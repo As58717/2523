990ec773778cb4413b019e15092e06b07c1c1e44
As58717 <i52de1fwc@hotmail.com>
2025-10-18 20:44:41 +0800

    Fix TextureRenderTargetResource include detection

--- Plugins/OmniCapture/Source/OmniCapture/Private/OmniCaptureEquirectConverter.cpp
+++ Plugins/OmniCapture/Source/OmniCapture/Private/OmniCaptureEquirectConverter.cpp
@@ -6,6 +6,8 @@
 #include "Rendering/TextureRenderTargetResource.h"
 #elif __has_include("Engine/TextureRenderTargetResource.h")
 #include "Engine/TextureRenderTargetResource.h"
+#elif __has_include("TextureRenderTargetResource.h")
+#include "TextureRenderTargetResource.h"
 #else
 class FTextureRenderTargetResource;
 #endif
