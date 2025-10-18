60eab6a1eaa938e12d044dd0cec3638eaed0ae8e
As58717 <i52de1fwc@hotmail.com>
2025-10-18 13:21:48 +0800

    Fix missing render target resource include

--- Plugins/OmniCapture/Source/OmniCapture/Private/OmniCaptureEquirectConverter.cpp
+++ Plugins/OmniCapture/Source/OmniCapture/Private/OmniCaptureEquirectConverter.cpp
@@ -2,6 +2,7 @@
 
 #include "Engine/TextureRenderTarget2D.h"
 #include "Engine/TextureRenderTarget.h"
+#include "Engine/TextureRenderTargetResource.h"
 #include "OmniCaptureTypes.h"
 
 #include "GlobalShader.h"
